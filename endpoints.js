c1d9b856f596c751a0584d7581a80dd9ecbec2fa
m-delamore <cosmicmuwanderer@gmail.com>
2024-04-04 18:55:11 +0800

    13 test cases able to run.
    Completed test. There are 13 test cases created which can be run on mocha. The mocha module is added to this repository so have to run mocha locally. Will need own review tomorrow before submission.

---
+++ endpoints.js
@@ -0,0 +1,76 @@
+
+// This function is to generate 10 random unique numbers.
+function generateTenNumbers(){
+    let randomNumberArray = [];
+
+    for (let i = 0; i < 10; i++) {
+        let randomNumber = Math.floor(Math.random() * 101); // To include 100
+        randomNumberArray.push(randomNumber);
+    }
+
+    return randomNumberArray;
+}
+
+function sortNumbers(arrayObj) {
+    
+    // Have to first ensure that there are only 10 items in the array.
+    if (arrayObj.length !== 10) {
+        throw new Error("Array must contain exactly 10 items only.");
+    }
+
+    // I used the Set() method to determine if the number of unique items are equal to 10.
+    // Placing the check to ensure there are 10 items only before this will ensure the unique items are counted correctly.
+    let unique = new Set(arrayObj);
+    if (unique.size !== 10) {
+        throw new Error("Items in array must contain 10 different numbers.");
+    }
+
+    // Ensure all items are number data type.
+    for (let num of arrayObj) {
+        if (typeof num !== 'number') {
+            throw new Error("There is one or more items in the array that are not numbers.");
+        }
+    }
+
+    // Ensure all items are numbers between 0 and 100. This will include ones with floating points.
+    for (let num of arrayObj) {
+        if (num < 0 || num > 100) {
+            throw new Error("All numbers in the array must be between 0 and 100.");
+        }
+    }
+    
+    // Arranging the numbers using sort method with the comparison callback function.
+    arrayObj.sort((a, b) => a - b);
+    
+    // Return the sorted array
+    return arrayObj;
+}
+
+function sumNumbers(num1, num2) {
+
+    // Ensure both arguments are of 'number' data type.
+    if (typeof num1 !== 'number' || typeof num1 !== 'number') {
+        throw new Error("There is one or more items in the array that are not numbers.");
+    }
+    
+    // Ensure both numbers are between 0 and 100
+    if (num1 < 0 || num1 > 100 || num2 < 0 || num2 > 100) {
+        throw new Error("Both numbers must be between 0 and 100.");
+    }
+    
+    // Calculate the sum of the two numbers
+    const sum = num1 + num2;
+    
+    // Return the sum
+    return sum;
+}
+
+
+// numbers = [100, 18, 25, 46, 81, 73, 20, 35, 1, 28]
+// generateTenNumbers();
+// console.log(sortNumbers(numbers));
+
+module.exports = {
+    sortNumbers: sortNumbers,
+    sumNumbers: sumNumbers
+};
---
+++ testEndpoints.js
@@ -0,0 +1,78 @@
+const assert = require('assert');
+const { sortNumbers, sumNumbers } = require('./endpoints.js');
+
+describe('sortNumbers', function() {
+    
+    it('is able to arrange an array of 10 unique numbers in ascending order', function() {
+        const input = [100, 18, 25, 46, 81, 73, 20, 35, 1, 28];
+        const expectedOutput = [1, 18, 20, 25, 28, 35, 46, 73, 81, 100];
+        assert.deepEqual(sortNumbers(input), expectedOutput);
+    });
+
+    it('throws an error if the array has more than 10 items', function() {
+        const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+        assert.throws(() => sortNumbers(input), Error);
+    });
+
+    it('throws an error if the array has less than 10 items', function() {
+        const input = [54, 72, 81, 9, 30];
+        assert.throws(() => sortNumbers(input), Error);
+    });
+
+    it('throws an error if the array contains identical numbers', function() {
+        const input = [1, 2, 3, 4, 5, 7, 7, 8, 9, 0];
+        assert.throws(() => sortNumbers(input), Error);
+    });
+
+    it('throws an error if the array contains numbers that are more than 100', function() {
+        const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 101];
+        assert.throws(() => sortNumbers(input), Error);
+    });
+
+    it('throws an error if the array contains numbers that are negative', function() {
+        const input = [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8];
+        assert.throws(() => sortNumbers(input), Error);
+    });
+
+    it('throws an error if the array contains strings', function() {
+        const input = ["hello", 0, 1, 2, 3, 4, 5, 6, 7, 8.9];
+        assert.throws(() => sortNumbers(input), Error);
+    });
+
+    it('throws an error if the array contains boolean', function() {
+        const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, true];
+        assert.throws(() => sortNumbers(input), Error);
+    });
+
+    it('throws an error if the array contains items that are of object type', function() {
+        const input = [0, 1, 2, 3, 4, 5, 6, 7, 8, [9, 10]];
+        assert.throws(() => sortNumbers(input), Error);
+    });
+
+    it('throws an error if the input is not an array object type', function() {
+        const input = 0;
+        assert.throws(() => sortNumbers(input), Error);
+    });
+});
+
+describe('sumNumbers', function() {
+    it('returns the correct sum of two numbers between 0 and 100', function() {
+        const num1 = 37;
+        const num2 = 99;
+        const expectedSum = num1 + num2;
+        output = sumNumbers(num1, num2);
+        assert.strictEqual(output, expectedSum);
+    });
+
+    it('throws an error if either number is not between 0 and 100', function() {
+        const num1 = 101;
+        const num2 = 37;
+        assert.throws(() => sumNumbers(num1, num2), Error);
+    });
+
+    it('throws an error if either number is negative', function() {
+        const num1 = 100;
+        const num2 = -37;
+        assert.throws(() => sumNumbers(num1, num2), Error);
+    });
+});
